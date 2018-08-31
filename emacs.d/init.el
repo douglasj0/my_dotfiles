dd92cdb9e8707ce16b0eaca27360e3fc023705a3
Douglas Jackson <djackson@grubhub.com>
2018-08-31 13:38:24 -0500

    Added use-package-verbose to enable showing package load times

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -27,7 +27,9 @@
   (package-refresh-contents)
   (package-install 'use-package))
 (eval-when-compile
-  (require 'use-package))
+  (require 'use-package)
+  (setq use-package-verbose t)
+)
 
 ;; Test out a package without installing it
 ;(use-package try
