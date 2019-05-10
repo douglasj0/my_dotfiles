ad65e59c596b39fb9d4ef09b9093d18f079bc6bb
Douglas Jackson <djackson@grubhub.com>
2019-05-10 10:54:16 -0500

    Updated encryption script and daily log

--- bin_mac/encfslogin.sh
+++ bin_mac/encfslogin.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
  
 ENCFS="/usr/local/bin/encfs" 
-ENCDIR="$HOME/Dropbox/.encrypted"
-DECDIR="$HOME/Documents/grubhub"
+#ENCDIR="$HOME/Dropbox/.encrypted"
+ENCDIR="$HOME/Dropbox/.private"
+#DECDIR="$HOME/Documents/grubhub"
+DECDIR="$HOME/Private"
  
-security find-generic-password -ga encfs 2>&1 >/dev/null | cut -d'"' -f2 | "$ENCFS" -o "volname=grubhub" -S "$ENCDIR" "$DECDIR"
+#security find-generic-password -ga encfs 2>&1 >/dev/null | cut -d'"' -f2 | "$ENCFS" -o "volname=grubhub" -S "$ENCDIR" "$DECDIR"
+security find-generic-password -ga encfs 2>&1 >/dev/null | cut -d'"' -f2 | "$ENCFS" -o "volname=Private" -S "$ENCDIR" "$DECDIR"
