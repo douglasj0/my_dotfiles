a40e37651a11f18e2c810b49950ec4767af35e2e
Douglas Jackson <djackson@grubhub.com>
2020-10-15 14:22:43 -0500

    Added in early-init.el for testing

---
+++ emacs.d/early-init.el
@@ -0,0 +1,14 @@
+;
+;
+; disabling those in your init file, but elements were loaded before your init file, so you HAVE to call scroll-bar-mode and tool-bar-mode, which can take quite a lot of time (more than 100ms on my laptop)
+
+;(push '(tool-bar-lines . 0) default-frame-alist)
+;(push '(menu-bar-lines . 0) default-frame-alist)
+;(push '(vertical-scroll-bars) default-frame-alist)
+
+(when (fboundp 'tool-bar-mode)
+  (tool-bar-mode -1))
+(when (fboundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
