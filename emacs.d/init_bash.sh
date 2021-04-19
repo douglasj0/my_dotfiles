d08bbf6b95d8e86eed5f67425f9dcfa96c123fea
Douglas Jackson <djackson@grubhub.com>
2021-04-18 21:58:20 -0500

    Big cleanup and re-organizing of .emacs.d.  Installed no-littering package and restructuring .emacs.d to work with it (moved to etc and var directories).

--- emacs.d/init_bash.sh
+++
@@ -1,3 +0,0 @@
-# Emacs shell-mode profile (M-x shell)
-echo -e "Sourcing from .emacs.d/init_bash.sh\n"
-PS1='\h:\W \u\$ '
--- emacs.d/themes/themes.el
+++
@@ -1,6 +0,0 @@
- ;;; Only use Themes in GUI. Set color theme (load-theme) for >= Emacs 24
- (when (display-graphic-p)
-   (if (fboundp 'load-theme)
-    (progn
-      (push "~/.emacs.d/themes/" custom-theme-load-path)
-      (load-theme 'zenburn))))
