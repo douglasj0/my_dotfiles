80860dd21cd246a9e11d7cbaaab7d2973ce26c81
Douglas Jackson <djackson@grubhub.com>
2021-04-24 14:51:34 -0500

    some more pre init startup additions, moved pkg logic to early-init.el

--- emacs.d/early-init.el
+++ emacs.d/early-init.el
@@ -1,4 +1,6 @@
 ;;; early-init.el --- -*- lexical-binding: t -*-
+;;
+;; Filename: early-init.el
 
 ;; ideasman_42's suggestion
 ;; https://www.reddit.com/r/emacs/comments/msll0j/do_any_of_you_have_some_tips_on_speeding_up_emacs/
@@ -6,7 +8,7 @@
 ;; default of gc-cons-threshold is 800k
 (defvar default-gc-cons-threshold 16777216 ; 16mb
   "my default desired value of `gc-cons-threshold'
-during normal emacs operations.")
+   during normal emacs operations.")
 
 ;; make garbage collector less invasive
 (setq
@@ -19,10 +21,9 @@ during normal emacs operations.")
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
 
-(setq
-  default-file-name-handler-alist
-  file-name-handler-alist
-  file-name-handler-alist nil)
+;;; Temporarily disable the file name handler.
+(setq default-file-name-handler-alist file-name-handler-alist)
+(setq file-name-handler-alist nil)
 
 (add-hook 'emacs-startup-hook
   (lambda (&rest _)
@@ -35,10 +36,17 @@ during normal emacs operations.")
     ;; delete no longer necessary startup variable
     (makunbound 'default-file-name-handler-alist)))
 
-;;; Disable package-enabel-at-startup sinc we're handling it
+;; Package initialize occurs automatically, before `user-init-file' is loaded, but after
+;; `early-init-file'. We handle package initialization, so we must prevent Emacs from doing it
+;; early!
 (setq package-enable-at-startup nil)
+(advice-add #'package--ensure-init-file :override #'ignore)
+
 ;;; and site-run-file
 (setq site-run-file nil)
+
+;; Avoid the pitfall of “loading old bytecode instead of newer source”
+(setq load-prefer-newer t)
 
 ;; Profile emacs startup
 ;; https://raw.githubusercontent.com/daviwil/dotfiles/master/Emacs.org
@@ -49,3 +57,45 @@ during normal emacs operations.")
          (float-time
             (time-subtract after-init-time before-init-time)))
       gcs-done)))
+
+
+
+;; Disable ad-redefinition-action messages on startup
+;; Caused by third party functions redefining defadvice
+;; https://andrewjamesjohnson.com/suppressing-ad-handle-definition-warnings-in-emacs/
+(setq ad-redefinition-action 'accept)
+
+;;; Ignore byte-compile warning
+;; ex. for Emacs 27: Warning: cl package required at runtime
+(setq byte-compile-warnings '(not nresolved
+                                  free-vars
+                                  callargs
+                                  redefine
+                                  obsolete
+                                  noruntime
+                                  cl-functions
+                                  interactive-only
+                                  ))
+
+;; Also suppress extraneous package-initialize errors
+(setq warning-suppress-log-types '((package reinitialization)))
+
+;; set elpa directory name based on emacs major version
+;(setq package-user-dir (format "~/.emacs.d/elpa-%d" emacs-major-version)) ;ex. elpa-27
+(setq package-user-dir (concat "~/.emacs.d/elpa-" emacs-version)) ;ex. elpa-27.1
+
+;; To refresh package list, run:  M-x package-refresh-contents
+;; To manually update installed packages:  M-x package-list-packages U x
+(require 'package)
+;(setq package-enable-at-startup nil) ; moved to early-init
+(setq package-archives
+      '(
+        ;("elpy"  . "http://jorgenschaefer.github.io/packages/") ; elpy package archive
+        ("gnu"   . "https://elpa.gnu.org/packages/") ; default package archive
+        ("org"   . "https://orgmode.org/elpa/") ; provides org-plus-contrib
+        ("melpa" . "https://melpa.org/packages/") ; milkypostman's pkg archive
+))
+
+(package-initialize)
+
+(provide 'early-init)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,23 +1,6 @@
 ;;; init.el --- -*- lexical-binding: t -*-
 ;;; lexical scope: https://nullprogram.com/blog/2016/12/22/
 
-;; set elpa directory name based on emacs major version
-;(setq package-user-dir (format "~/.emacs.d/elpa-%d" emacs-major-version)) ;ex. elpa-27
-(setq package-user-dir (concat "~/.emacs.d/elpa-" emacs-version)) ;ex. elpa-27.1
-
-;; To refresh package list, run:  M-x package-refresh-contents
-;; To manually update installed packages:  M-x package-list-packages U x
-(require 'package)
-(setq package-enable-at-startup nil)
-(setq package-archives
-      '(
-        ;("elpy"  . "http://jorgenschaefer.github.io/packages/") ; elpy package archive
-        ("gnu"   . "https://elpa.gnu.org/packages/") ; default package archive
-        ("org"   . "https://orgmode.org/elpa/") ; provides org-plus-contrib
-        ("melpa" . "https://melpa.org/packages/") ; milkypostman's pkg archive
-))
-
-(package-initialize)
 
 ;; install use-package early, needed for org-plus-contrib
 ;; https://github.com/jwiegley/use-package
@@ -25,27 +8,15 @@
   (package-refresh-contents) ;; maybe only do before installing pkgs?
   (package-install 'use-package))
 
-;; Disable ad-redefinition-action messages on startup
-;; Caused by third party functions redefining defadvice
-;; https://andrewjamesjohnson.com/suppressing-ad-handle-definition-warnings-in-emacs/
-(setq ad-redefinition-action 'accept)
-
-;;; Ignore byte-compile warning
-;; ex. for Emacs 27: Warning: cl package required at runtime
-(setq byte-compile-warnings '(not nresolved
-                                  free-vars
-                                  callargs
-                                  redefine
-                                  obsolete
-                                  noruntime
-                                  cl-functions
-                                  interactive-only
-                                  ))
+;; install no-littering as early as possible
+; Help keeping ~/.emacs.d clean
+; https://github.com/emacscollective/no-littering/
+(use-package no-littering               ; Keep .emacs.d clean
+  :ensure t
+)
 
 ;; Install and load the latest org-mode version via org-plus-contrib
 (use-package org
-  ;:defer t
-  ;; to be sure we have the latest Org version
   :ensure org-plus-contrib
   :hook
   ;(org-mode . variable-pitch-mode)
@@ -53,13 +24,6 @@
   ;(org-mode . org-num-mode)
   :custom
   (org-src-tab-acts-natively t))
-
-;; install no-littering as early as possible
-; Help keeping ~/.emacs.d clean
-; https://github.com/emacscollective/no-littering/
-(use-package no-littering               ; Keep .emacs.d clean
-  :ensure t
-)
 
 ;; Simplify tangling, on emacs startup check
 (when (file-exists-p "~/.emacs.d/emacs-init.org")
